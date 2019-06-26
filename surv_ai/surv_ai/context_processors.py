42e12929b1d7adaeabc03bcdfb3d8c35db3cd19f
Marco Marinello <marco.marinello@school.rainerum.it>
2019-06-26 14:22:34 +0200

    Add a context processor to provide the site title
    Signed-off-by: Marco Marinello <marco.marinello@school.rainerum.it>

--- surv_ai/surv_ai/context_processors.py
+++ surv_ai/surv_ai/context_processors.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2019 Valerio Maggio <vmaggio@fbk.eu>
+# Copyright (c) 2019 Marco Marinello <marco.marinello@school.rainerum.it>
 
 from django.conf import settings
 
@@ -9,3 +11,10 @@ def app_metadata(request):
     if settings.INDEX_ALT_LOGO_TEXT:
         metadata['index_alt_logo_text'] = settings.INDEX_ALT_LOGO_TEXT
     return metadata
+
+
+def site_title(request):
+    ctx = dict()
+    if hasattr(settings, "ADMIN_SITE_TITLE"):
+        ctx["ADMIN_SITE_TITLE"] = settings.ADMIN_SITE_TITLE
+    return ctx
